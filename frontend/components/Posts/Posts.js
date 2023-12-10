bce51b676a629b21137ea803da9f0dccc5d2c060
Jacob Handley <skaarjshark@outlook.com>
2023-12-10 14:38:25 -0600

    module 2 challenge

---
+++ frontend/components/Posts/Posts.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Post from './Post';
+
+const Posts = (props) => {
+  // 🔥 Make sure the parent of Posts is passing the right props!
+  const { likePost, posts } = props;
+
+  return (
+    <div className='posts-container-wrapper'>
+      {/* Map through the posts array returning a Post component at each iteration */}
+      {/* Check the implementation of Post to see what props it requires! */}
+      {
+        posts.map(pst => <Post key={pst.id} post={pst} likePost={likePost}/>)
+      }
+    </div>
+  );
+};
+
+export default Posts;
